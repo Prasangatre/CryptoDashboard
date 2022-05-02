7a58eb8949abb74cb485b981715083b29b61a058
Prasang <prasangatre260@gmail.com>
2022-05-02 22:26:46 +0530

    firstcommit

---
+++ src/config/data.js
@@ -0,0 +1,19 @@
+export const chartDays = [
+    {
+      label: "24 Hours",
+      value: 1,
+    },
+    {
+      label: "7 Days",
+      value: 7,
+    },
+    {
+      label: "3 Months",
+      value: 90,
+    },
+    {
+      label: "1 Year",
+      value: 365,
+    },
+  ];
+  
